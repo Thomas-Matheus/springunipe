e5fdc7cb56a8a1bb04d2615921c62abd8987cdc9
ravelmello <ravelmello@hotmail.com>
2016-11-12 09:27:50 -0300

    Adicionando verbos de get com id e post

--- src/main/java/com/spring/boot/unipe/BookController.java
+++ src/main/java/com/spring/boot/unipe/BookController.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,28 @@ public class BookController {
 		listFakeBook.add(new Book(4,"Angular 2","The boys","1232131231"));
 		
 		return new ResponseEntity<List<Book>>(listFakeBook, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/books/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Book> getBooks(@PathVariable String id){
+		
+		Book book = new Book(1,"Software Architecture", "I don't know","123HAUDHoODJA");
+		
+		return book == null ? 
+				new ResponseEntity<Book>(HttpStatus.NOT_FOUND):
+					new ResponseEntity<Book>(book,HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/book/add", method=RequestMethod.POST)
+	public ResponseEntity<Book> createBook(@RequestBody Book bookTitle){
+		try {
+			
+			return new ResponseEntity<Book>(HttpStatus.CREATED);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			return new ResponseEntity<Book>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 	
 }
